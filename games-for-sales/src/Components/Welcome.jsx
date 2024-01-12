4a97e52bab8418e88a5215cf3203346f7dd437de
Gianni <giannicimmino1996@gmail.com>
2024-01-12 03:07:41 +0100

    Commit

---
+++ games-for-sales/src/Components/Welcome.jsx
@@ -0,0 +1,12 @@
+import Alert from "react-bootstrap/Alert";
+
+function Welcome() {
+  return (
+    <Alert variant="success">
+      <Alert.Heading>Hey, piacere di vederti</Alert.Heading>
+      <p>Benvenuto nella mia applicazione. Qui troverai una lista di libri.</p>
+    </Alert>
+  );
+}
+
+export default Welcome;
